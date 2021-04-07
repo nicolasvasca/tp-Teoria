ad03142ce4eaf6ecc14c60d9075281bf795bb522
Nicolas Vasca <nicolas.vasca@usemobile.xyz>
2021-04-07 09:34:29 -0300

    feat: add funcao for read external file txt

--- main.cpp
+++ main.cpp
@@ -4,12 +4,5 @@ int main(){
     Processamento *processamento = new Processamento();
     string nome = "afd1.txt";
     lerArquivo(nome,processamento);
-
-    vector<string> palavras = processamento->getPalavras();
-    for(int i = 0; i < palavras.size();i++){
-        cout << palavras[i] <<endl;
-    }
-    
-
     return 1;
 }
