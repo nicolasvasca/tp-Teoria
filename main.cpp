3a18c9afbde158313ad6e29dbe8628cf370bf513
Nicolas Vasca <nicolas.vasca@usemobile.xyz>
2021-04-07 09:52:07 -0300

    refactor: funcao to lerArquivo

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
-#include "funcao.h"
+#include "lerArquivo.h"
 
 int main(){
     Processamento *processamento = new Processamento();
     string nome = "afd1.txt";
     lerArquivo(nome,processamento);
+
+    vector<string> palavras = processamento->getPalavras();
+    for(int i = 0; i < palavras.size();i++){
+        cout << palavras[i] <<endl;
+    }
+    
+
     return 1;
 }
